fbb06290f76fd97f48e98009d7234dbb88218306
Your Name <your@email.com>
2026-04-15 00:06:35 +0545

    Version 4

--- service/emailOtpProvider.js
+++ service/emailOtpProvider.js
@@ -6,12 +6,12 @@ export const transporter = nodemailer.createTransport({
   port: 587,
   secure: false,
   family: 4,
+  tls: {
+    rejectUnauthorized: false
+  },
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  },
-  tls: {
-    rejectUnauthorized: false
   }
 })
 
