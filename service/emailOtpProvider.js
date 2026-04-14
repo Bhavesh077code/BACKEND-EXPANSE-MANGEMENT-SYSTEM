6fbc93598a9f7d4e9133459f07a8ee828b38832a
Your Name <your@email.com>
2026-04-15 00:00:35 +0545

    Version 4

--- service/emailOtpProvider.js
+++ service/emailOtpProvider.js
@@ -2,10 +2,10 @@ import nodemailer from "nodemailer";
 import "dotenv/config"
 
 export const transporter = nodemailer.createTransport({
-  //service: "gmail",
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, // 🔥 IMPORTANT
+  secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
