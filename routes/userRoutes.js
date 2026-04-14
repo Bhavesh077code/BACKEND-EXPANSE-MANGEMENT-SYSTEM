6c3f9eaa15a274dd59f80211abebeb959c58d360
Your Name <your@email.com>
2026-04-14 22:33:41 +0545

    Versio 3

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,12 @@
 import express from "express";
 import { sendOtp } from "../controller/userController.js";
 import { verifyOtp } from "../controller/verifyOtpController.js";
-import authMiddleware from "../middleware/authMiddleware.js";
-import rateLimit from "express-rate-limit";
 
 
 const router = express.Router();
 
-router.post("/send-otp", rateLimit, sendOtp)
-router.post("/verify-otp", rateLimit, verifyOtp);
+router.post("/send-otp",  sendOtp)
+router.post("/verify-otp", verifyOtp);
 
 
 export default router;
